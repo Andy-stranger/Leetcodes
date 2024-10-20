d7035d41de1ff969cd4718922f7bf9029762fc49
DESKTOP-MHCL4AU\User <anandhagani2001@gmail.com>
2024-10-20 23:55:24 +0530

    first push

--- ConsoleApplication/LeetCodes/obj/Debug/net8.0/LeetCodes.AssemblyInfo.cs
+++ ConsoleApplication/LeetCodes/obj/Debug/net8.0/LeetCodes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23809fc6ecc9cad4a10d1e2b52ee8bdccd72c7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
